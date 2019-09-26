dfea8a14436a76d935ab6b2daad5eba2e8950b62
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-09-26 22:30:52 +0300

    Add random generation of people db

---
+++ lab_1/generateDB.py
@@ -0,0 +1,31 @@
+from faker import Faker
+from random import randint
+from random import uniform
+from random import choice
+
+MAX_N = 1000
+
+sex = ['m', 'f']
+
+
+def generate_people():
+    faker = Faker()
+    f = open('people.csv', 'w')
+    for i in range(MAX_N):
+        age = randint(0, 111)
+        h = randint(40, 250)
+        w = h * h * 0.00225 + randint(0, 15)
+        calorie_norm = randint(1000,3000)
+        line = "{0},{1},{2},{3},{4},{5}\n".format(faker.name(),
+                                                  choice(sex),
+                                                  age,
+                                                  h,
+                                                  int(w),
+                                                  calorie_norm)
+        f.write(line)
+    f.close()
+
+if __name__ == "__main__":
+    generate_people()
+        
+        

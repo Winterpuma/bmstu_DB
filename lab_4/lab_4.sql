06a6637d2e0f1e8a49fcd51df8a25bf2c9844a32
Winterpuma <32524078+Winterpuma@users.noreply.github.com>
2019-12-22 18:06:47 +0300

    Add task 4, 5, 6 in lab_4

--- lab_4/lab_4.sql
+++ lab_4/lab_4.sql
@@ -46,3 +46,62 @@ $$ language plpython3u;
 select * from get_people_by_age(18);
 select * from get_people_by_age(-1);
 select * from get_people_by_age(25);
+
+
+-- Добавляет нового человека в базу
+create or replace procedure add_person(name varchar,  sex varchar,  age int) as
+$$
+plan = plpy.prepare("insert into people(name, sex, age) values($1, $2, $3);", ["varchar", "varchar", "int"])
+plpy.execute(plan, [name,  sex,  age])
+$$ language plpython3u;
+
+call add_person('Test person', 'm', 33);
+
+select * from people
+order by id desc;
+
+
+-- При удалении людей, данные копируются в таблицу people_back
+create or replace function backup_deleted_people()
+returns trigger 
+as $$
+plan = plpy.prepare("insert into people_back(name, sex, age, height, weight, calories) values($1, $2, $3, $4, $5, $6);", ["varchar", "varchar", "int", "int", "float", "int"])
+pi = TD['old']
+rv = plpy.execute(plan, [pi["name"], pi["sex"], pi["age"], pi["height"], pi["weight"], pi["calories"]])
+return TD['new']
+$$ language  plpython3u;
+
+drop trigger backup_deleted_people on people; 
+
+create trigger backup_deleted_people
+before delete on people for each row
+execute procedure backup_deleted_people();
+
+delete from people
+where name = 'Test person';
+
+select * from people_back;
+
+
+-- Тип параметров человека
+create type stats as (
+  age int,
+  height int,
+  weight real
+);
+
+drop function get_stats_by_id;
+drop type player_cut;
+-- Вывод параметров человека по id
+create or replace function get_stats_by_id(id_ integer) returns stats 
+as $$
+plan = plpy.prepare("select age, height, weight from people where id = $1", ["int"])
+cr = plpy.execute(plan, [id_])
+return (cr[0]['age'], cr[0]['height'], cr[0]['weight'])
+$$ language plpython3u;
+
+select * from get_stats_by_id(1001);
+
+select *
+from people
+where id = 1001
